fd940ceae996341e8014d3fc8414b93bda51d3df
Shota Sato <last11142@gmail.com>
2024-05-06 20:50:18 +0900

    add: sample json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-function App() {
+import { Box, Container } from "@yamada-ui/react";
 
+function App() {
   return (
     <>
-      <div>sample</div>
+      <Box w="full" h="full" bgColor="white">
+        <Container
+          w="80%"
+          minH="2xl"
+          marginInline="auto"
+          marginBlock="xl"
+          border="1px solid"
+          borderColor="blackAlpha.400"
+          borderRadius="xl"
+        >
+          main contents
+        </Container>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
